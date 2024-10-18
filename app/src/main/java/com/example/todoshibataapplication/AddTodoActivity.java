57099706e71a64f4da30a796c720f60816623f66
Shibata Yosei (柴田 遥成) <Yosei.Shibata@jp.ricoh.com>
2024-10-18 09:04:36 +0900

    仮完成

--- app/src/main/java/com/example/todoshibataapplication/AddTodoActivity.java
+++ app/src/main/java/com/example/todoshibataapplication/AddTodoActivity.java
@@ -1,13 +1,17 @@
 package com.example.todoshibataapplication;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class AddTodoActivity extends AppCompatActivity {
     private EditText todoTitleEditText;
@@ -23,6 +27,7 @@ public class AddTodoActivity extends AppCompatActivity {
         todoDetailsEditText = findViewById(R.id.todo_details);
         todoDueDateEditText = findViewById(R.id.todo_due_date);
         Button saveTodoButton = findViewById(R.id.save_todo_button);
+        Button selectDueDateButton = findViewById(R.id.select_due_date_button);
         Button canselButton = findViewById(R.id.cancel_button);
 
         saveTodoButton.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +52,26 @@ public class AddTodoActivity extends AppCompatActivity {
                 resultIntent.putExtra("newTodo", newTodo);
                 setResult(RESULT_OK, resultIntent);
                 finish();
+            }
+        });
+
+        selectDueDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                // DatePickerDialogを表示
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddTodoActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
+                        String dueDate = String.format("%04d-%02d-%02d", selectedYear, selectedMonth + 1, selectedDay);
+                        todoDueDateEditText.setText(dueDate);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
             }
         });
 
